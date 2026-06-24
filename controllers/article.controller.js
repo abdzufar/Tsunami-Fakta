ab9187ba390d798b813ab8c9c25dc8f00963127a
Yogasmara <septianayoga30@gmail.com>
2026-06-24 22:10:44 +0700

    adding articles details

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -1,14 +1,46 @@
-const { Article } = require("../models");
+const { Article, User, Category } = require("../models");
 const cutContent = require("../helpers/cutContent");
+const { Op } = require("sequelize");
 
 class Controller {
   static async renderArticlePage(req, res) {
     try {
       let data = await Article.findAll();
-      console.log(data);
 
       res.render("articles", { data, cutContent });
     } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async getDetailArticle(req, res) {
+    try {
+      const { id } = req.params;
+
+      let categoriesData = await Category.findAll();
+
+      let data = await Article.findByPk(id, {
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      });
+      // let data = await Article.findAll({
+      //   include: {
+      //     model: User,
+      //     attributes: ["username"],
+      //   },
+      //   where: {
+      //     id: {
+      //       [Op.eq]: id,
+      //     },
+      //   },
+      // });
+      console.log(categoriesData);
+
+      res.render("detailArticle", { data });
+    } catch (error) {
+      console.log(error);
       res.send(error);
     }
   }
--- routes/article.route.js
+++ routes/article.route.js
@@ -3,4 +3,6 @@ const Controller = require("../controllers/article.controller");
 
 router.get("/", Controller.renderArticlePage);
 
+router.get("/:id", Controller.getDetailArticle);
+
 module.exports = router;
