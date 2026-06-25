d99833d5694b53d7012bdbc4d87c0e851816d3a7
abdzufar <zufarzufar@gmail.com>
2026-06-25 11:53:55 +0700

    add currentUser session

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+require('dotenv').config();
 const express = require("express");
+const session = require('express-session');
 const app = express();
 const router = require("./routes");
 const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    sameSite: true
+  }
+}));
 app.use(router);
 
 app.listen(port, () => {
